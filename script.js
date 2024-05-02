72729f6a7c5c6c1c374532c0eceeb8b813510c08
AmrinderSinghKhangura <amrinder.khangura@edu.sait.ca>
2024-05-02 19:49:40 -0400

    delete, clear, and dynamic filter and clear all button

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const addBtn = document.querySelector("button");
 const input = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 
+function CheckEmptyList() {
+  const filterInput = document.getElementById("filter");
+  const clearbtn = document.querySelector("#clear");
+  const items = document.querySelectorAll("li");
+  items.length !== 0
+    ? ((filterInput.style.display = "block"),
+      (clearbtn.style.display = "block"))
+    : ((filterInput.style.display = "none"), (clearBtn.style.display = "none"));
+}
+
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (input.value.trim() === "") {
@@ -21,6 +31,42 @@ addBtn.addEventListener("click", (e) => {
   delBtn.appendChild(i);
   newLiItem.appendChild(delBtn);
   itemList.appendChild(newLiItem);
-
+  CheckEmptyList();
   input.value = "";
 });
+
+//FIRST WAY TO REMOVE
+// const ulButtons = document.querySelectorAll("ul button");
+// ulButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const listItem = button.parentElement;
+//     listItem.remove();
+//   });
+//   CheckEmptyList();
+// });
+
+//Second way to remove
+itemList.addEventListener("click", function (e) {
+  e.target.parentElement.classList.contains("remove-item") &&
+    confirm("Are you sure?") &&
+    e.target.parentElement.parentElement.remove();
+
+  CheckEmptyList();
+});
+
+//clear button
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", () => {
+  //first way
+  //   const choppingList = document.querySelectorAll("ul li");
+  //   choppingList.forEach((li) => li.remove());
+
+  // second way
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  CheckEmptyList();
+});
+
+CheckEmptyList();
